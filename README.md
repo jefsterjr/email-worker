21fa7e6415cff613639378af075371924b61d7c1
JefsterFarlei <jefster.farlei@itsstecnologia.com.br>
2020-12-15 14:44:21 -0300

    Implementação final do projeto

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # email-worker
+
+Projeto desenvolvido durante o curso de Docker para aprendizado.
--- app/app.sh
+++ app/app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pip install bottle==0.12.13
+pip install bottle==0.12.13 psycopg2==2.8.6 redis==2.10.5
 python -u sender.py
